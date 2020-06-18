733f1b0066e7fcbe1e930ae2efe49f7ed99653b8
ashleigh-lyman <ajlyman@my365.bellevue.edu>
2020-06-18 09:04:58 -0500

    added new files and folder

---
+++ week-8/Lyman_calculator.py
@@ -0,0 +1,20 @@
+#============================================
+# Title:  Assignment 8.3 - Calculator
+# Author: Richard Krasso
+# Date:   17 June 2020
+# Description: Exercise 8.3 - Calculator
+#===========================================
+
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1/num2
+
+print(add(1,2))
+print(subtract(4,1))
+print(divide(8,2))
---
+++ week-8/Lyman_hello_world.py
@@ -0,0 +1,16 @@
+#============================================
+#; Title: Assignment 8.2
+#; Author: Ashleigh Lyman
+#; Date: 16 June 2020
+#; Modified By:
+#; Description: Working with Python
+#;===========================================
+
+
+#Header
+#var header = require('../Lyman-header');
+#console.log(header.display('Ashleigh', 'Lyman', 'Exercise 8.2 - Hello World'));
+
+
+
+print('Hello World')
